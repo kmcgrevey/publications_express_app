5e9a0a3332a606ece1b1724636d78bcb539cf5ec
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-24 12:42:21 -0600

    Gets a specific paper by id

--- server.js
+++ server.js
@@ -83,3 +83,20 @@ app.post('/api/v1/footnotes', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+// gets a specific paper
+app.get('/api/v1/papers/:id', (request, response) => {
+  database('papers').where('id', request.params.id).select()
+    .then(papers => {
+      if (papers.length) {
+        response.status(200).json(papers);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
